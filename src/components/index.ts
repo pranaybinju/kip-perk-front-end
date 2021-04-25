238344de010ed7571ddf5657eab7fc2044e85116
Nipun Paradkar <nipun@kiprosh.com>
2021-04-25 20:50:44 +0530

    Fix index file for components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,33 @@
-import Login from './Login';
-import Register from './Register';
-import Reset from './Reset';
-import Dashboard from './Dashboard';
+import Button from './Button';
 import Claim from './Claim';
+import Container from './Container';
+import Dashboard from './Dashboard';
+import FormLabel from './FormLabel';
+import Heading from './Heading';
 import Home from './Home';
+import IconButton from './IconButton';
+import Image from './Image';
+import Input from './Input';
+import Modal from './Modal';
+import Tag from './Tag';
+import Text from './Text';
+import Textarea from './Textarea';
+import VerificationForm from './VerificationForm';
 
 export {
-  Login,
-  Register,
-  Reset,
-  Dashboard,
+  Button,
   Claim,
-  Home
+  Container,
+  Dashboard,
+  FormLabel,
+  Heading,
+  Home,
+  IconButton,
+  Image,
+  Input,
+  Modal,
+  Tag,
+  Text,
+  Textarea,
+  VerificationForm
 };
